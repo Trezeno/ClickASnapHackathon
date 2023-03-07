036eb210fac2dca0acfbf03ebce9afd15d5d47cf
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-03-07 17:56:40 +0000

    Full button functionality implemented. Each new image searched for is store, previous image button iterates backwards over the previously seen images, followed by next image button either iterating back through the list to number of images seen, or if at most newly generated one, continues to generate more.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let seenImagesNumbers = [];
+let seenImagesList = [["IMAGE NUMBER", "IMAGE TAG", "USER SUB STATUS"]];
 let currentImageNumber = 0;
 let photoTagList = ["FOOTBALL", "SPACE", "FLOWERS", "COMPUTERS", "GAMING", "ANIMALS", "FASHION", "CELEBRITIES", "FOOD", "SCENERY"]
 let userInterestsStarter = "Current user interests: ";
@@ -16,10 +16,10 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-//Function that will weight each new image with a 20% chance of being uploaded by a free user vs 80% chance from a paid user
+//Function that will weight each new image with a 10% chance of being uploaded by a free user vs 90% chance from a paid user
 function getRandomImageSub() {
   let randomImageWeight = Math.random();
-  if (randomImageWeight >= 0.8) {
+  if (randomImageWeight >= 0.9) {
     return "FREE USER";
   } else {
     return "PAID USER";
@@ -28,41 +28,54 @@ function getRandomImageSub() {
 
 //Functions for getting next and previous images
 function nextImage() {
-  if (userInterests.length >= 1) { //This will be if user has custom interests
-    currentImageNumber += 1;
+  if (currentImageNumber < seenImagesList.length - 1) { //This is to check whether we are revisiting an already passed image number
+    currentImageNumber++;
     document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
-    let newImageTag = userInterests[getRandomInt(userInterests.length)];
-    document.getElementById("image-tag").innerHTML = newImageTag;
-    let newSubStatus = getRandomImageSub();
-    document.getElementById("sub-status").innerHTML = newSubStatus;
-    console.log("CUSTOM INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
-  } else { //This will be if no user interests have been selected
-    currentImageNumber += 1;
-    document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
-    let newImageTag = photoTagList[getRandomInt(photoTagList.length)];
-    document.getElementById("image-tag").innerHTML = newImageTag;
-    let newSubStatus = getRandomImageSub();
-    document.getElementById("sub-status").innerHTML = newSubStatus;
-    console.log("NO INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
+    document.getElementById("image-tag").innerHTML = seenImagesList[currentImageNumber][1];
+    document.getElementById("sub-status").innerHTML = seenImagesList[currentImageNumber][2];
+  } else { //Otherwise this is acting as finding new images for us
+    if (userInterests.length >= 1) { //This will be if user has custom interests
+      currentImageNumber += 1;
+      document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+      let newImageTag = userInterests[getRandomInt(userInterests.length)];
+      document.getElementById("image-tag").innerHTML = newImageTag;
+      let newSubStatus = getRandomImageSub();
+      document.getElementById("sub-status").innerHTML = newSubStatus;
+      console.log("CUSTOM INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
+      seenImagesList.push([currentImageNumber, newImageTag, newSubStatus]);
+      console.table(seenImagesList);
+    } else { //This will be if no user interests have been selected
+      currentImageNumber += 1;
+      document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+      let newImageTag = photoTagList[getRandomInt(photoTagList.length)];
+      document.getElementById("image-tag").innerHTML = newImageTag;
+      let newSubStatus = getRandomImageSub();
+      document.getElementById("sub-status").innerHTML = newSubStatus;
+      console.log("NO INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
+      seenImagesList.push([currentImageNumber, newImageTag, newSubStatus]);
+      console.table(seenImagesList);
+    }
   }
 }
 
-//This will need to be updated once saving seen images shown to be able to revert to EXACTLY what was previously shown
+
+/* This will need to be updated once saving seen images shown to be able to revert to EXACTLY what was previously shown
+ * First number in array seach is index of image number
+ * Second number in array search is 0 / 1 / 2
+ * 0 == Image Number
+ * 1 == Image Tag
+ * 2 == User Sub Status
+ */
 function prevImage() {
-  if (currentImageNumber > 1) {
-    currentImageNumber -= 1;
-    document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
-    console.log("PREV SUCCESS, new number: " + currentImageNumber)
-  } else {
+  if (currentImageNumber == 0) {
+    window.alert("Please click 'NEXT IMAGE' to get started.")
+  } else if (currentImageNumber == 1) {
     console.log("FAIL, at image number 1")
+  } else {
+    currentImageNumber--;
+    document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+    document.getElementById("image-tag").innerHTML = seenImagesList[currentImageNumber][1];
+    document.getElementById("sub-status").innerHTML = seenImagesList[currentImageNumber][2];
   }
 }
 
-//To store each seen image so when we click previous we will iterate over the last seen images, and next will go back until end of seen images where we will then start to find more new images of course
-class Image {
-  seenImage(number, tag, sub) {
-    this.number = number;
-    this.tag = tag;
-    this.sub = sub;
-  }
-}
