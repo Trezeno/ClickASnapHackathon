fad181fbc1a61e84fcb9e92a54ea154fb0fef1e3
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-03-07 12:10:24 +0000

    button numbers basic change sorted

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let seenImagesNumbers = [];
 let currentImageNumber = 1;
 
-
-function prevImage() {
-}
-
 function nextImage() {
+  currentImageNumber += 1;
+  document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+  console.log("SUCCESS, new number: " + currentImageNumber)
 }
+
+//This will need to be updated once saving seen images shown to be able to revert to EXACTLY what was previously shown
+function prevImage() {
+  if (currentImageNumber > 1) {
+    currentImageNumber -= 1;
+    document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+    console.log("SUCCESS, new number: " +currentImageNumber)
+  } else {
+    console.log("FAIL, at image number 1")
+  }
+}
+
+
+
