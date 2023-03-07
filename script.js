45fe649673c50b09cdb7f8addaf100fb631d553e
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-03-07 16:42:03 +0000

    next image button fully complete i think

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let seenImagesNumbers = [];
-let currentImageNumber = 1;
+let currentImageNumber = 0;
 let photoTagList = ["FOOTBALL", "SPACE", "FLOWERS", "COMPUTERS", "GAMING", "ANIMALS", "FASHION", "CELEBRITIES", "FOOD", "SCENERY"]
 let userInterestsStarter = "Current user interests: ";
 let userInterests = [];
 
+//Function that gets and stores user interests
 function getUserInterests() {
   userInterests.push(prompt("Enter one of your interests: ").toUpperCase());
   console.log("Updated user interests: " + userInterests);
   document.getElementById("interests").innerHTML = userInterestsStarter + userInterests.join(", ");
 }
 
+//Function that will choose a random index within an array (using for photoTagList and userInterests)
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+//Function that will weight each new image with a 20% chance of being uploaded by a free user vs 80% chance from a paid user
+function getRandomImageSub() {
+  let randomImageWeight = Math.random();
+  if (randomImageWeight >= 0.8) {
+    return "FREE USER";
+  } else {
+    return "PAID USER";
+  }
 }
 
 //Functions for getting next and previous images
@@ -21,13 +33,17 @@ function nextImage() {
     document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
     let newImageTag = userInterests[getRandomInt(userInterests.length)];
     document.getElementById("image-tag").innerHTML = newImageTag;
-    console.log("CUSTOM INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag);
+    let newSubStatus = getRandomImageSub();
+    document.getElementById("sub-status").innerHTML = newSubStatus;
+    console.log("CUSTOM INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
   } else { //This will be if no user interests have been selected
     currentImageNumber += 1;
     document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
     let newImageTag = photoTagList[getRandomInt(photoTagList.length)];
     document.getElementById("image-tag").innerHTML = newImageTag;
-    console.log("NO INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag);
+    let newSubStatus = getRandomImageSub();
+    document.getElementById("sub-status").innerHTML = newSubStatus;
+    console.log("NO INTERESTS NEXT SUCCESS, new number: " + currentImageNumber + " new tag: " + newImageTag + " new user: " + newSubStatus);
   }
 }
 
