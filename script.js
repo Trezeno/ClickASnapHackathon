403d0eca1aa40cf622cf1544020dcacdc3f3762b
Trezeno <113196678+Trezeno@users.noreply.github.com>
2023-03-07 15:31:02 +0000

    condensed css code and added some if/else to next button

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ function getUserInterests() {
   userInterests.push(prompt("Enter one of your interests: ").toUpperCase());
   console.log("Updated user interests: " + userInterests);
   document.getElementById("interests").innerHTML = userInterestsStarter + userInterests.join(", ");
-  
 }
 
+
+//Functions for getting next and previous images
 function nextImage() {
-  currentImageNumber += 1;
-  document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
-  console.log("SUCCESS, new number: " + currentImageNumber)
+  if (userInterests.length >= 1) { //This will be if user has custom interests
+      currentImageNumber += 1;
+      document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+      console.log("CUSTOM INTERESTS SUCCESS, new number: " + currentImageNumber)
+  } else { //This will be if no user interests have been selected
+      currentImageNumber += 1;
+      document.getElementById("image-number").innerHTML = "IMAGE " + currentImageNumber;
+      console.log("NO INTERESTS SUCCESS, new number: " + currentImageNumber)
+  }
 }
 
 //This will need to be updated once saving seen images shown to be able to revert to EXACTLY what was previously shown
